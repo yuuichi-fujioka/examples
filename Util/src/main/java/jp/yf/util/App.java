98f1f498ce56cf197682308191ffe1a01db50e11
yuuichi-fujioka <fujioka.yuuichi@gmail.com>
2013-03-29 17:04:17 +0900

    add slf4j

--- Util/src/main/java/jp/yf/util/App.java
+++ Util/src/main/java/jp/yf/util/App.java
@@ -1,5 +1,8 @@
 package jp.yf.util;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Hello world!
  *
@@ -8,6 +11,8 @@ public class App
 {
     public static void main( String[] args )
     {
+    	Logger logger = LoggerFactory.getLogger(App.class);
+    	logger.info("hello world");
         System.out.println( "Hello World!" );
     }
 }
